51aaff178a568c696891224fcfd569dd41df46b6
Aman Kumar <authorisedaman@gmail.com>
2024-06-09 23:55:54 +0530

    add

--- server/config/database.js
+++ server/config/database.js
@@ -2,14 +2,16 @@ import mongoose from 'mongoose';
 import { configDotenv } from 'dotenv';
 configDotenv();
 
-const dbConnect = async () => {
-  await mongoose
+const dbConnect =  () => {
+   mongoose
     .connect(process.env.DATABASE_URL)
     .then(() => {
       console.log('database has been connected');
     })
     .catch((error) => {
+      console.log(error);
       console.log('not able to connect to the database');
+      process.exit(1);
     });
 };
 export default dbConnect;
